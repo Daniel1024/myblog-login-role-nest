5cf3dd3ddb51d439f965c3e3830fa88d935e4d11
Daniel Lopez <d.lopez.1740@gmail.com>
2021-04-21 17:17:35 -0400

    config .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,31 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+import { AppService } from './app.service';
+import { AppController } from './app.controller';
 import { PostModule } from './post/post.module';
 import { UserModule } from './user/user.module';
+import * as Db from './config/constants';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'myblog_user',
-      password: 'myblog_password',
-      database: 'myblog_db',
-      entities: [__dirname + './**/**/*entity{.ts,.js}'],
-      autoLoadEntities: true,
-      synchronize: true,
-      logging: true,
-      logger: 'file',
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        host: config.get(Db.DATABASE_HOST),
+        port: config.get<number>(Db.DATABASE_PORT),
+        username: config.get(Db.DATABASE_USERNAME),
+        password: config.get(Db.DATABASE_PASSWORD),
+        database: config.get(Db.DATABASE_NAME),
+        entities: [__dirname + './**/**/*entity{.ts,.js}'],
+        autoLoadEntities: true,
+        synchronize: true,
+        logging: true,
+        logger: 'file',
+      })
     }),
     PostModule,
     UserModule
